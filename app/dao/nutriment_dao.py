7296f1a86314e22fe2f010e76af9ef14d0353db3
christ <bj791956@gitlab.info.uqam.ca>
2026-03-20 11:49:54 -0400

    update structure backend

---
+++ app/dao/nutriment_dao.py
@@ -0,0 +1,16 @@
+##############################################################
+#   doa/nutriment_dao.py
+#
+#- Rôle : Gérer les nutriments des produits.
+#- Exemples :
+#
+#   get_nutriments_by_product()
+#   add_nutriment_of_product()
+#   remove_nutriment_of_product()
+#   update_nutriment_of_product()
+# 
+#
+#-  Entrée : id produit
+#-  Sortie : liste de nutriments.
+#
+#############################################################
