acede887202d16868e04f95b18433e74f1339af0
Hugues Aimé <hugues@MacBook-Pro-de-Hugues.local>
2026-03-30 16:53:59 -0400

    ajout ingredients_original_tags

--- etl-airflow/dags/off-initial-load-dag.py
+++ etl-airflow/dags/off-initial-load-dag.py
@@ -92,7 +92,7 @@ TRANSFORMED_FILE_KEY = f"{DAG_ID}/data_transformed.parquet"
 FILTER_COLUMNS = ",".join([
     "code", "brands", "product_name", "product_quantity", "product_quantity_unit",
     "quantity", "serving_quantity", "serving_size", "categories_tags", "countries_tags",
-    "ecoscore_score", "ecoscore_grade", "images", "ingredients_tags", "ingredients",
+    "ecoscore_score", "ecoscore_grade", "images", "ingredients_original_tags",
     "nutriscore_score", "nutriscore_grade", "nutriments",
 ])
 
