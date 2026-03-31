207da208bf081eaea42b88c998593f1b8234ed64
Hugues Aimé <hugues@MacBook-Pro-de-Hugues.local>
2026-03-30 22:23:06 -0400

    maj

--- etl-images/config/target_columns.py
+++ etl-images/config/target_columns.py
@@ -14,7 +14,7 @@ TARGET_COLUMNS = [
     'countries_tags',
     'ecoscore_score',
     'ecoscore_grade',
-    'ingredients_tags',
+    'ingredients_normalized',
     'nutriscore_score',
     'nutriscore_grade',
     'front_url',
@@ -35,6 +35,6 @@ TARGET_COLUMNS = [
     'calcium_100g',
     'iron_100g',
     'potassium_100g',
-    'ingredients_normalized'
+    
     
 ]
